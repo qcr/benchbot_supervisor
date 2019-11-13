625b4b0e8b04ecada9fab5f453ba4c4a22e0c9f4
ben <b.talbot@qut.edu.au>
2019-11-13 16:30:36 +1000

    Add support for blind passive angle movement

--- src/supervisor_callbacks.py
+++ src/supervisor_callbacks.py
@@ -16,6 +16,14 @@ _MOVE_DISTANCE_SPEED = 0.5
 _MOVE_HZ = 20
 
 
+def __pi_wrap(angle):
+    return np.mod(angle + np.pi, 2 * np.pi) - np.pi
+
+
+def __safe_dict_get(d, key, default):
+    return d[key] if type(d) is dict and key in d else default
+
+
 def __transform_stamped_to_matrix(tfs):
     # FFS ROS... how do you still not have a method for this in 2019...
     return t3.affines.compose([
@@ -72,18 +80,37 @@ def encode_pose(data, supervisor):
     pass
 
 
-def move_angle(data, pubisher, supervisor):
-    pass
+def move_angle(data, publisher, supervisor):
+    # TODO handle collisions
+    angle = __pi_wrap(__safe_dict_get(data, 'angle', 0))
+    vel_msg = Twist(angular=Vector3(z=np.sign(angle) * _MOVE_ANGLE_SPEED))
+    hz_rate = rospy.Rate(_MOVE_HZ)
+    try:
+        # Yes, this is pretty gross top to bottom. We have used a gross sign
+        # factor to make our while case consistent between +ve & -ve angles,
+        # and used some wrapping to hope our flimsy sign assumptions will hold
+        # for us.  Valid simplifying, or bug introduction? Time will tell...
+        start = current_pose(data, supervisor)
+        current = start
+        positive = (1 if angle >= 0 else -1)  # Yes, gross way to handle -ves
+        print("Moving to angle %f (hack factor: %d)" % (angle, positive))
+        print("Moved angle is: %f" %
+              t3.euler.mat2euler(__pose_diff_matrices(start, current))[2])
+        while (positive * __pi_wrap(angle - t3.euler.mat2euler(
+                __pose_diff_matrices(start, current))[2]) > 0):
+            publisher.publish(vel_msg)
+            hz_rate.sleep()
+            current = current_pose(data, supervisor)
+    except Exception as e:
+        rospy.logerr("move_angle: failed with exception '%s'" % e)
+    publisher.publish(Twist())
 
 
 def move_distance(data, publisher, supervisor):
     # TODO handle collisions
-    print("Received: %s" % data)
-    vel_msg = Twist(linear=Vector3(x=_MOVE_DISTANCE_SPEED))
+    distance = __safe_dict_get(data, 'distance', 0)
+    vel_msg = Twist(linear=Vector3(x=np.sign(distance) * _MOVE_DISTANCE_SPEED))
     hz_rate = rospy.Rate(_MOVE_HZ)
-    distance = (data['distance']
-                if type(data) is dict and 'distance' in data else 0)
-    print("Moving distance of: %f" % distance)
     try:
         start = current_pose(data, supervisor)
         current = start
