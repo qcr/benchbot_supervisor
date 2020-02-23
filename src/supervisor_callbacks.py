2e1e34ef1bea68e973185c21d47bf94962793fd9
david2611 <david.r.hall.2611@gmail.com>
2020-02-24 09:33:48 +1000

    bug fix hstack

--- src/supervisor_callbacks.py
+++ src/supervisor_callbacks.py
@@ -67,7 +67,7 @@ def __tr_b_wrt_a(matrix_a, matrix_b):
 def __transrpy_to_tf_matrix(trans, rpy):
     # Takes a translation vector & roll pitch yaw vector
     return __pose_vector_to_tf_matrix(
-        np.hstack(Rot.from_euler('XYZ',rpy).as_quat(), trans))
+        np.hstack((Rot.from_euler('XYZ',rpy).as_quat(), trans))
 
 
 def __yaw_b_wrt_a(matrix_a, matrix_b):
