29a51c2574216b93bd681669440227922f3b8736
ben <b.talbot@qut.edu.au>
2021-02-17 13:03:41 +1000

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='benchbot_supervisor',
-    version='1.1.0',
+    version='2.0.0',
     author='Ben Talbot',
     author_email='b.talbot@qut.edu.au',
     description='The BenchBot Supervisor, used in the BenchBot Software Stack',
